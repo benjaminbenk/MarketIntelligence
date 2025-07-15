a03a7c7a08a3ef8d945f277f3ea7df75f8770ad1
benjaminbenk <benjamin.benko@evk.hu>
2025-07-15 22:19:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -267,7 +267,6 @@ if st.session_state.get("show_entry_modal", False):
       <p><strong>Source:</strong> {row.get('Name','N/A')}</p>
       <!-- real link that reloads with close_modal=1 -->
       <a href="{close_href}" class="modal-close" target="_top">
-          ⬅️ Back to Summary
       </a>
     </div>
     """
